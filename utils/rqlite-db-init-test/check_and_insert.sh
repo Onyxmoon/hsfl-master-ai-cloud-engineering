53bc7b9e549bb48593b90d5f017c6545b5e099ba
Philipp Borucki <p.borucki@icloud.com>
2024-01-12 14:56:54 +0100

    feat(utils): add util for adding test data

---
+++ utils/rqlite-db-init-test/check_and_insert.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+RQLITE_URL="http://${RQLITE_HOST}:${RQLITE_PORT}"
+CHECK_SQL="SELECT COUNT(*) FROM user;"
+
+TABLE_EMPTY=$(curl -u "${RQLITE_USER}":"${RQLITE_PASSWORD}" -s -G "${RQLITE_URL}/db/query" --data-urlencode "q=${CHECK_SQL}" | jq '.results[0].values[0][0]' -r)
+
+if echo "$TABLE_EMPTY" | grep -qE '^[0-9]+$' ; then
+    if [ "$TABLE_EMPTY" -gt 0 ]; then
+        echo "Database not reachable or not untouched. No changes will be made."
+        exit 1
+    fi
+fi
+curl -u "${RQLITE_USER}":"${RQLITE_PASSWORD}" -s -XPOST "${RQLITE_URL}/db/load" -H "Content-type: text/plain" --data-binary @init.sql
